34aed6395e941063bbe4768b4b00533f42dd0f2b
farhadmpr <farhadmp@gmail.com>
2020-02-24 14:53:10 +0330

    Add Ef DbContext

---
+++ MyStore/MyStore.Infrastructures.Dal/Commons/MyStoreContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyStore.Core.Domain.Categories;
+using MyStore.Core.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Infrastructures.Dal.Commons
+{
+    public class MyStoreContext : DbContext
+    {
+        public MyStoreContext(DbContextOptions<MyStoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
---
+++ MyStore/MyStore.Infrastructures.Dal/Products/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyStore.Core.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Infrastructures.Dal.Products
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasIndex(c => c.Name).IsUnique();
+            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Description).HasMaxLength(500);
+
+        }
+    }
+}
