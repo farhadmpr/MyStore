ec35af2a10ee116acd41d6fd5e83d32a3f782f42
farhadmpr <farhadmp@gmail.com>
2020-02-24 15:08:25 +0330

    Add category configuration

---
+++ MyStore/MyStore.Infrastructures.Dal/Categories/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyStore.Core.Domain.Categories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Infrastructures.Dal.Categories
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasIndex(c => c.CategoryName).IsUnique();
+            builder.Property(c => c.CategoryName).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category { CategoryId = 1, CategoryName = "Cat 01" },
+                new Category { CategoryId = 2, CategoryName = "Cat 02" }
+                );
+        }
+    }
+}
