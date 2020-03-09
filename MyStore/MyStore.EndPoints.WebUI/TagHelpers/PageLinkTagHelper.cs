64525df66fc793b72ccb0ca3735f5ed224ab6e6e
farhadmpr <farhadmp@gmail.com>
2020-03-09 14:03:05 +0330

    Add TagHelper for paging

---
+++ MyStore/MyStore.EndPoints.WebUI/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyStore.EndPoints.WebUI.Models.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.EndPoints.WebUI.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        // برای ساخت آدرس ها و یو آر ال ها
+        private readonly IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        // در هر محیطی مثلا کنترولر یا ویو یک تعداد اطلاعاتی وجود دارد
+        // که فریم ورک در اختیار ما قرار می دهد
+        // پراپرتی اینجکشن
+        // فریم ورک ویو کانتکست را در پراپرتی با نام ویو کانتکست اینجکت می کند
+        // از طریق انوتیشن بالایی مشخص کرده ایم
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        // برای پارامتر های جستجو
+        public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>();
+
+        // برای سی اس اس
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                PageUrlValues["pageNumber"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
+
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                output.Content.AppendHtml(tag);
+            }
+        }
+
+
+    }
+}
