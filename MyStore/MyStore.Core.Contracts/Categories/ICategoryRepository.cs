3d715cf7e6dcf88c69e87c3eba8b492fd86c43ad
farhadmpr <farhadmp@gmail.com>
2020-03-09 14:14:49 +0330

    Add CategoryRepository

---
+++ MyStore/MyStore.Core.Contracts/Categories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyStore.Core.Domain.Categories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Core.Contracts.Categories
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetAll();
+    }
+}
---
+++ MyStore/MyStore.Infrastructures.Dal/Categories/EfCategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyStore.Core.Contracts.Categories;
+using MyStore.Core.Domain.Categories;
+using MyStore.Infrastructures.Dal.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyStore.Infrastructures.Dal.Categories
+{
+    public class EfCategoryRepository : ICategoryRepository
+    {
+        private readonly MyStoreContext _ctx;
+
+        public EfCategoryRepository(MyStoreContext mySotreContext)
+        {
+            _ctx = mySotreContext;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _ctx.Categories.ToList();
+        }
+    }
+}
