75d9ab24d7826b051738c128dff8c75414949f11
farhadmpr <farhadmp@gmail.com>
2020-03-15 10:12:44 +0330

    Add Cart & CartLine models

---
+++ MyStore/MyStore.Core.Domain/Carts/CartLine.cs
@@ -0,0 +1,15 @@
+﻿using MyStore.Core.Domain.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Core.Domain.Carts
+{
+    public class CartLine
+    {
+        public int CartLineId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
