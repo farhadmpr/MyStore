eb6089a80370e41aa52aec80971570f2f93d5909
farhadmpr <farhadmp@gmail.com>
2020-03-25 10:42:44 +0430

    Add Order entity

---
+++ MyStore/MyStore.Core.Domain/Orders/Order.cs
@@ -0,0 +1,29 @@
+﻿using MyStore.Core.Domain.Carts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyStore.Core.Domain.Orders
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public ICollection<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage ="please enter a name")]
+        public string Name { get; set; }
+
+        public string Line1{ get; set; }
+        public string Line2{ get; set; }
+        public string City { get; set; }
+        public bool GiftWrap { get; set; }
+
+
+        public string PaymentId { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public bool Shipped { get; set; }
+
+
+    }
+}
---
+++ MyStore/MyStore.EndPoints.WebUI/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyStore.EndPoints.WebUI.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
