d071236fe24f69fa181d0f8b58c467a152d8d23e
farhadmpr <farhadmp@gmail.com>
2020-03-25 10:19:30 +0430

    Add CartSummary ViewComponent

---
+++ MyStore/MyStore.EndPoints.WebUI/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Core.Domain.Carts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.EndPoints.WebUI.Components
+{
+    public class CartSummaryViewComponent:ViewComponent
+    {
+        private Cart _cart;
+
+        public CartSummaryViewComponent(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_cart);
+        }
+    }
+}
