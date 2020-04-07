a438eb4fb87859fdf4bb595db9b2c771591685ee
farhadmpr <farhadmp@gmail.com>
2020-04-07 14:53:27 +0430

    Add Pay service

--- MyStore/MyStore.Services.ApplicationServices/Payments/PayIrPayment.cs
+++ MyStore/MyStore.Services.ApplicationServices/Payments/PayIrPayment.cs
@@ -1,11 +1,45 @@
-﻿using MyStore.Core.Contracts.Payments;
+﻿using Microsoft.Extensions.Configuration;
+using MyStore.Core.Contracts.Payments;
+using MyStore.Core.Domain.Payments;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace MyStore.Services.ApplicationServices.Payments
 {
     public class PayIrPayment : IPayment
     {
+        private readonly IConfiguration configuration;
+
+        public PayIrPayment(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public RequestPaymentResult Pay(string amount, string mobile, string factorNumber, string description)
+        {
+            HttpClient client = new HttpClient();
+            Dictionary<string, string> postValues = new Dictionary<string, string>();
+            
+            postValues.Add("api", configuration["PayIr:ApiKey"]);
+            postValues.Add("amount", amount);
+            postValues.Add("redirect", configuration["PayIr:RedirectUrl"]);
+            postValues.Add("mobile", mobile);
+            postValues.Add("factorNumber", factorNumber);
+            postValues.Add("description", description);
+
+            var content = new FormUrlEncodedContent(postValues);
+            var response = client.PostAsync(configuration["PayIr:RequestUrl"], content).Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<RequestPaymentResult>(responseString);
+        }
+
+        public VerifyPaymentResult Verify(string transId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
